f61112e15a875722d6546ad09b6b272a9018a1cc
pountzas <nikos@pountzas.gr>
2021-08-06 20:52:06 +0300

    create asynnc get scores from api function

--- src/Game.js
+++ src/Game.js
@@ -1,2 +1,9 @@
 const GAME_ID = 'EtQZjbZctTfC60wlEG9f';
 
+class Game {
+  constructor(id) {
+    this.id = id;
+    this.url = `
+      https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/${id}/scores/
+    `;
+  }
