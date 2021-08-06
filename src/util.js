4a814cf33d2eb0fd5d09bf8147f1a4085ef44dea
pountzas <nikos@pountzas.gr>
2021-08-06 20:54:50 +0300

    add import myGame and selectors

---
+++ src/util.js
@@ -0,0 +1,6 @@
+import myGame from './Game.js';
+
+const scoreList = document.querySelector('.score-list');
+const inputName = document.querySelector('.input-name');
+const inputScore = document.querySelector('.input-score');
+
