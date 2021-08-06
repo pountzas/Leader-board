75bd0a530ccc2ac37fc0f2028a32539ab14a3570
pountzas <nikos@pountzas.gr>
2021-08-06 20:55:21 +0300

    add function renderList

--- src/util.js
+++ src/util.js
@@ -4,3 +4,16 @@ const scoreList = document.querySelector('.score-list');
 const inputName = document.querySelector('.input-name');
 const inputScore = document.querySelector('.input-score');
 
+const renderList = () => {
+  scoreList.innerHTML = '';
+  myGame.getScores()
+    .then((scores) => {
+      scores.result.forEach((score, index) => {
+        scoreList.innerHTML += `
+          <li class="score-item${index % 2 === 0 ? ' item-shade' : ''}">
+            ${score.user}: ${score.score}
+          </li>
+        `;
+      });
+    });
+};
